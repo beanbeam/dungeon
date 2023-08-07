fd338601d63da03a3056e602496c0298924104e5
Colin Clark <cclark0409@gmail.com>
2023-08-07 12:00:03 -0400

    Add wall textures

--- js/app/dungeon.js
+++ js/app/dungeon.js
@@ -39,47 +39,67 @@ define(['lib/three', 'constants', 'party', 'direction'], function(THREE, Const, 
         },
 
         addMeshesToScene: function(scene) {
-            var material = new THREE.MeshPhongMaterial({color: 0x888888});
+            var texLoader = new THREE.TextureLoader();
+            var map = this.map
+            texLoader.load('textures/brick/diffuse.png', function(diffuse) {
+                diffuse.magFilter = THREE.NearestFilter
+                diffuse.minFilter = THREE.NearestFilter
+                diffuse.wrapS = diffuse.wrapT = THREE.RepeatWrapping;
+                diffuse.repeat.set(1, 1);
+                
+                texLoader.load('textures/brick/normal.png', function(normal) {
+                    normal.magFilter = THREE.NearestFilter
+                    normal.minFilter = THREE.NearestFilter
+                    normal.wrapS = normal.wrapT = THREE.RepeatWrapping;
+                    normal.repeat.set(1, 1);
 
-            var basicWallGeom = new THREE.BoxGeometry(1, 1, 1);
-            var basicTileGeom = new THREE.PlaneGeometry(1, 1, 1);
-            for (var y = 0; y < this.map.length; y++) {
-                var row = this.map[y];
-                for (var x = 0; x < row.length; x++) {
-                    var type = row[x];
+                    var material = new THREE.MeshPhongMaterial({
+                        map: diffuse,
+                        normalMap: normal,
+                        shininiess: 1
+                    });
 
-                    if (type == 0) {
-                        var mesh = new THREE.Mesh(basicWallGeom, material);
-                        mesh.castShadow = true;
-                        mesh.receiveShadow = true;
+                    var basicWallGeom = new THREE.BoxGeometry(1, 1, 1);
+                    var basicTileGeom = new THREE.PlaneGeometry(1, 1, 1);
+                    for (var y = 0; y < map.length; y++) {
+                        var row = map[y];
+                        for (var x = 0; x < row.length; x++) {
+                            var type = row[x];
 
-                        mesh.position.x = x;
-                        mesh.position.y = 0.5;
-                        mesh.position.z = y;
-                        scene.add(mesh);
-                    } else if (type == 1 || type == 2) {
-                        var floorMesh = new THREE.Mesh(basicTileGeom, material);
-                        floorMesh.castShadow = true;
-                        floorMesh.receiveShadow = true;
+                            if (type == 0) {
+                                var mesh = new THREE.Mesh(basicWallGeom, material);
+                                mesh.castShadow = true;
+                                mesh.receiveShadow = true;
 
-                        floorMesh.position.x = x;
-                        floorMesh.position.y = 0;
-                        floorMesh.position.z = y;
-                        floorMesh.rotation.x = Math.PI*-0.5;
-                        scene.add(floorMesh);
+                                mesh.position.x = x;
+                                mesh.position.y = 0.5;
+                                mesh.position.z = y;
+                                scene.add(mesh);
+                            } else if (type == 1 || type == 2) {
+                                var floorMesh = new THREE.Mesh(basicTileGeom, material);
+                                floorMesh.castShadow = true;
+                                floorMesh.receiveShadow = true;
 
-                        var ceilMesh = new THREE.Mesh(basicTileGeom, material);
-                        ceilMesh.castShadow = true;
-                        ceilMesh.receiveShadow = true;
+                                floorMesh.position.x = x;
+                                floorMesh.position.y = 0;
+                                floorMesh.position.z = y;
+                                floorMesh.rotation.x = Math.PI*-0.5;
+                                scene.add(floorMesh);
 
-                        ceilMesh.position.x = x;
-                        ceilMesh.position.y = 1;
-                        ceilMesh.position.z = y;
-                        ceilMesh.rotation.x = Math.PI*0.5;
-                        scene.add(ceilMesh);
+                                var ceilMesh = new THREE.Mesh(basicTileGeom, material);
+                                ceilMesh.castShadow = true;
+                                ceilMesh.receiveShadow = true;
+
+                                ceilMesh.position.x = x;
+                                ceilMesh.position.y = 1;
+                                ceilMesh.position.z = y;
+                                ceilMesh.rotation.x = Math.PI*0.5;
+                                scene.add(ceilMesh);
+                            }
+                        }
                     }
-                }
-            }
+                });
+            });
         }
     };
 
